d4933b566424f529ba4b6c78bbf318b3867841f8
Fraimo <oleg441234@yandex.by>
2023-12-05 01:48:59 +0300

    Работа с БД выносится в отдельный класс

--- Program.cs
+++ Program.cs
@@ -4,18 +4,20 @@ using System.Data;
 using Microsoft.Data.SqlClient;
 using Microsoft.IdentityModel.Protocols;
 
-
 namespace LearnMsSql
 {
     // контейнерезация подключения к БД, прочтитать как это правильно делать
     // Ругается среда когда в public методе есть у переменной private static string connectionString  = ConfigurationManager.ConnectionStrings["DT"].ConnectionString;
     // private static SqlConnection sqlConnection = null;
+    // какой подход лучше? запись базы в код или лучше напрямую делать запросы в sql ? В ado.net Есть классы которые напрямую работают // Пока буду делать через отдлеьные запросы sql запросы :)
+
+
 
     internal class Program
     {
-        public static int ShowMenu()
+        public static void ShowMenu()
         {
-       
+
             Console.WriteLine("1.Добавить слово");
             Console.WriteLine("2.Тренировка");
             Console.WriteLine("3.Редактировать "); // Подумать как !
@@ -28,89 +30,71 @@ namespace LearnMsSql
             // Тут можно попытаться реализовать как в примерах с обобщеннем 
             int intChoise = 0;
 
-            if (int.TryParse(choise, out intChoise)) {}
+            if (int.TryParse(choise, out intChoise)) { }
             else
             {
                 Console.WriteLine("Неккоретно введенное значение! Необходимо ввести чесло соответствующее одному из пунктов меню");
             }
 
-            Console.ReadKey();
-
-            return intChoise;
+            GetСhoiceMenu(intChoise);
+            // return intChoise;
         }
 
-        // private static string connectionString  = ConfigurationManager.ConnectionStrings["DT"].ConnectionString;
 
-       // private static SqlConnection sqlConnection = null;
+        //private static void SqlConnect()
+        //{
+        //    // В Ado net пока понятно что придется создавать большой класс где будет происходить подключение к БД и реализация комманд с работой в БД
+        //    string connectionString = ConfigurationManager.ConnectionStrings["DT"].ConnectionString;
 
-        private static void SqlConnect()
-        {
-            // В Ado net пока понятно что придется создавать большой класс где будет происходить подключение к БД и реализация комманд с работой в БД
-            string connectionString = ConfigurationManager.ConnectionStrings["DT"].ConnectionString;
+        //    SqlConnection sqlConnection = new SqlConnection(connectionString);
 
-            SqlConnection sqlConnection = new SqlConnection(connectionString);
+        //    sqlConnection.Open();
 
-            sqlConnection.Open();
+        //     static void WriteWordInDB(SqlConnection sqlConnection, string rusWord, string polWord)
+        //    {
 
-             static void WriteWordInDB(SqlConnection sqlConnection, string rusWord, string polWord)
-            {
-                SqlCommand sqlCommand = new SqlCommand($"INSERT INTO [Table] (Rus_Name, Pol_Name) VALUES (N'{rusWord}', '{polWord}')", sqlConnection);
-                Console.WriteLine(sqlCommand.ExecuteNonQuery());
+        //        SqlCommand sqlCommand = new SqlCommand($"INSERT INTO [Table] (Rus_Name, Pol_Name) VALUES (N'{rusWord}', '{polWord}')", sqlConnection);
+        //        Console.WriteLine(sqlCommand.ExecuteNonQuery());
 
 
-            }
+        //    }
 
-            sqlConnection.Close();
+        //    sqlConnection.Close();
 
-            // sqlConnection.Close();
-        }
+        //}
 
         static void Main(string[] args)
         {
-            SqlConnect();
-            addWord();
-            // sqlConnection = new SqlConnection(connectionString);
-           // sqlConnection.Open();
 
-
-             // addWord();
-
-
-          // sqlConnection.Close();
+            ShowMenu();
 
 
         }
 
-        public static void addWord()
+        public static void ReadWord() // нужна обработка входных значений слов что бы туда попадали только слова
         {
             #region 
             Console.WriteLine("Для добавления нового слова в словарь вам необходимо ввести слова на русском и на польском");
             Console.WriteLine("Введите слово на русском");
             Console.Write(">");
-            string rusWord =  Console.ReadLine();
+            string rusWord = Console.ReadLine();
             Console.WriteLine("Введите слово на польском");
             Console.Write(">");
             string polWord = Console.ReadLine();
             Console.WriteLine($"Добавить новую пару слов: {rusWord} с переводом {polWord}?");
-            Console.Write(">");
-            string anwer = Console.ReadLine(); // добавить обработку набуквы y и n 
+            //Console.Write(">");
+            //string anwer = Console.ReadLine();
             #endregion
 
-
-           // SqlCommand sqlCommand = new SqlCommand($"INSERT INTO [Table] (Rus_Name, Pol_Name) VALUES (N'{rusWord}', '{polWord}')" ,sqlConnection);
-
-            // Console.WriteLine(sqlCommand.ExecuteNonQuery());
-
+            DBModificatet.WriteWordInDB(rusWord, polWord);
         }
 
         public static void GetСhoiceMenu(int menuChoice)
         {
             switch (menuChoice)
             {
-                case 0:
-                    addWord();
-                    break;
                 case 1:
+                    ReadWord();
                     break;
                 case 2:
                     break;
@@ -118,21 +102,53 @@ namespace LearnMsSql
                     break;
                 case 4:
                     break;
+                case 5:
+                    break;
                 default:
                     break;
             }
         }
 
-        public static void ToExit ()
+        public static void EditCoupleOfWords()
         {
-            //sqlConnection.Close();
-            Environment.Exit(0); 
+            // Слова могут щахотет
+            Console.WriteLine("");
+        }
+
+        public static void ToExit()
+        {
+            Environment.Exit(0);
 
         }
 
-        
+
+    }
+
+    
+
+    class DBModificatet
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["DT"].ConnectionString;
+
+        static SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+
+       public static void WriteWordInDB(string rusWord, string polWord)
+        {
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand($"INSERT INTO [Table] (Rus_Name, Pol_Name) VALUES (N'{rusWord}', '{polWord}')", sqlConnection);
+
+            Console.WriteLine(" Запись - ", sqlCommand.ExecuteNonQuery());
+
+            sqlConnection.Close();
+
+
+        }
+
     }
 }
+
 
 #region
 /*
