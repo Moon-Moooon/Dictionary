e010f7be61e8719cc493e31c208ff49520f9be85
Fraimo <oleg441234@yandex.by>
2023-12-05 07:38:07 +0300

    Добавлен метод обработки ввеленого русского слова, и отдельно выведен блок кода который отвечает за парсинг введеной цифры при выборе меню.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Configuration;
 using System.Data;
 using Microsoft.Data.SqlClient;
 using Microsoft.IdentityModel.Protocols;
+using System.Text;
 
 namespace LearnMsSql
 {
@@ -10,7 +11,7 @@ namespace LearnMsSql
     // Ругается среда когда в public методе есть у переменной private static string connectionString  = ConfigurationManager.ConnectionStrings["DT"].ConnectionString;
     // private static SqlConnection sqlConnection = null;
     // какой подход лучше? запись базы в код или лучше напрямую делать запросы в sql ? В ado.net Есть классы которые напрямую работают // Пока буду делать через отдлеьные запросы sql запросы :)
-
+    // Необходимо обрабатывать на входе слова, что бы точно быть уверенным что используется русский и польский языки
 
 
     internal class Program
@@ -20,7 +21,7 @@ namespace LearnMsSql
 
             Console.WriteLine("1.Добавить слово");
             Console.WriteLine("2.Тренировка");
-            Console.WriteLine("3.Редактировать "); // Подумать как !
+            Console.WriteLine("3.Редактировать "); 
             Console.WriteLine("4.Удалеине слова");
             Console.WriteLine("5.Выход");
 
@@ -28,56 +29,58 @@ namespace LearnMsSql
 
             string choise = Console.ReadLine();
             // Тут можно попытаться реализовать как в примерах с обобщеннем 
+            //int intChoise = 0;
+
+            //if (int.TryParse(choise, out intChoise)) { }
+            //else
+            //{
+            //    Console.WriteLine("Неккоретно введенное значение! Необходимо ввести чесло соответствующее одному из пунктов меню");
+            //}
+
+            //GetСhoiceMenu(intChoise);
+            // return intChoise;
+        }
+
+        public static void ParceInputMenuCHoise(string choise)
+        {
+
             int intChoise = 0;
 
-            if (int.TryParse(choise, out intChoise)) { }
+            if (int.TryParse(choise, out intChoise))
+            {
+                GetСhoiceMenu(intChoise);
+            }
             else
             {
                 Console.WriteLine("Неккоретно введенное значение! Необходимо ввести чесло соответствующее одному из пунктов меню");
             }
 
-            GetСhoiceMenu(intChoise);
-            // return intChoise;
         }
 
-
-        //private static void SqlConnect()
-        //{
-        //    // В Ado net пока понятно что придется создавать большой класс где будет происходить подключение к БД и реализация комманд с работой в БД
-        //    string connectionString = ConfigurationManager.ConnectionStrings["DT"].ConnectionString;
-
-        //    SqlConnection sqlConnection = new SqlConnection(connectionString);
-
-        //    sqlConnection.Open();
-
-        //     static void WriteWordInDB(SqlConnection sqlConnection, string rusWord, string polWord)
-        //    {
-
-        //        SqlCommand sqlCommand = new SqlCommand($"INSERT INTO [Table] (Rus_Name, Pol_Name) VALUES (N'{rusWord}', '{polWord}')", sqlConnection);
-        //        Console.WriteLine(sqlCommand.ExecuteNonQuery());
-
-
-        //    }
-
-        //    sqlConnection.Close();
-
-        //}
 
         static void Main(string[] args)
         {
 
-            ShowMenu();
+            // ShowMenu();
 
+           // Console.WriteLine(s.ToLower());   
 
         }
 
-        public static void ReadWord() // нужна обработка входных значений слов что бы туда попадали только слова
+        public static bool AnalysisRussianWord(string rusWord)
         {
+
+            return true;
+        }
+
+        public static void ReadWord() // нужна обработка входных значений слов что бы туда попадали только слова
+        {                              // Если вдруг разхотелось слово добовлять !
             #region 
             Console.WriteLine("Для добавления нового слова в словарь вам необходимо ввести слова на русском и на польском");
             Console.WriteLine("Введите слово на русском");
             Console.Write(">");
             string rusWord = Console.ReadLine();
+            ExeminationRusWord(rusWord, out rusWord);
             Console.WriteLine("Введите слово на польском");
             Console.Write(">");
             string polWord = Console.ReadLine();
@@ -89,7 +92,46 @@ namespace LearnMsSql
             DBModificatet.WriteWordInDB(rusWord, polWord);
         }
 
-        public static void GetСhoiceMenu(int menuChoice)
+        public static string ExeminationRusWord(string rusWord, out string warningChar)
+        {
+            string word = rusWord.ToLower();
+
+            warningChar = string.Empty;
+
+            byte[] listUtf8 = Encoding.Default.GetBytes(rusWord);
+
+            int j = 1;
+
+            for (int i = 0; i < listUtf8.Length; i++)
+            {
+                if (
+                   (listUtf8[i] >= 208 && listUtf8[i] <= 209) &&
+                   ((176 <= listUtf8[j] && listUtf8[j] <= 191) || (listUtf8[j] >= 128 && listUtf8[j] <= 143) || (listUtf8[j] == 145))
+                   )
+                { }
+
+                else
+                {
+                    return null;
+                }
+
+                i++;
+                j += 2;
+            }
+
+            return word;
+        }
+
+        public static void getAfterBedCHois (string warningChar)
+        {
+            Console.WriteLine($"Символ {warningChar} - является некорректным");
+            Console.WriteLine("Хотите добавить слово еще раз ?");
+            string anwer = Console.ReadLine();  
+
+        }
+
+
+        public static void GetСhoiceMenu(int menuChoice )
         {
             switch (menuChoice)
             {
@@ -111,8 +153,11 @@ namespace LearnMsSql
 
         public static void EditCoupleOfWords()
         {
-            // Слова могут щахотет
+            // Слова могут попытаться начать искать и на русском и на польском 
+            // Слово на русском - записали - 
             Console.WriteLine("");
+
+
         }
 
         public static void ToExit()
